491bf10fe7ed724e8b8ac9492d08db6571ea07ac
Tasnimsomo <tasnasomo@gmail.com>
2025-01-09 11:39:54 +0400

    server file

--- server.js
+++ server.js
@@ -1,39 +1,21 @@
 const express = require('express');
 const app = express();
 
-const users = [];
-
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
+
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+
+app.use('/', loginRouter);
+app.use('/', registerRouter);
+
+
 app.listen(3000, () => {
     console.log('server started');
 });
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'John' });
-});
 
-app.get('/login', (req, res) =>{
-    res.render('login.ejs');
-});
 
-app.get('/register', (req, res) =>{
-    res.render('register.ejs');
-});
 
-app.post('/register', async (req, res) =>{
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-        res.redirect('/login');
-    }
-    catch {
-        res.redirect('/register');
-    }
-    console.log(users);
-});
+
+
